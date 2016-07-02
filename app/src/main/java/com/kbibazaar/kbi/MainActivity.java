b6b0facc5c2a0086b359113d215198aa6b1a89bb
mukul <mukulraw199517@gmail.com>
2016-07-02 11:57:59 +0530

    asdasdasd

--- app/src/main/java/com/kbibazaar/kbi/MainActivity.java
+++ app/src/main/java/com/kbibazaar/kbi/MainActivity.java
@@ -3,6 +3,7 @@ package com.kbibazaar.kbi;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,29 +11,55 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView myWebView;
+    private WebSettings webSettings;
+    private ProgressBar progress;
 
-    @SuppressLint({"JavascriptInterface", "SetJavaScriptEnabled"})
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
+
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED , WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+
+        progress = (ProgressBar)findViewById(R.id.progress);
 
         onNewIntent(getIntent());
 
         myWebView = (WebView)findViewById(R.id.webView);
 
-        assert myWebView != null;
-        myWebView.getSettings().setJavaScriptEnabled(true);
+        if (myWebView != null) {
+            webSettings = myWebView.getSettings();
+        }
+
+        webSettings.setJavaScriptEnabled(true);
+
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        //webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        myWebView.setLayerType(View.LAYER_TYPE_HARDWARE , null);
+       // webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+
+   /*     myWebView.getSettings().setJavaScriptEnabled(true);
         myWebView.getSettings().setLoadWithOverviewMode(true);
         myWebView.getSettings().setUseWideViewPort(true);
         myWebView.getSettings().setBuiltInZoomControls(true);
@@ -41,6 +68,9 @@ public class MainActivity extends AppCompatActivity {
         myWebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
         myWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         myWebView.setLayerType(View.LAYER_TYPE_HARDWARE , null);
+*/
+
+
 
 
         //override the web client to open all links in the same webview
@@ -56,15 +86,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class MyWebViewClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+
+        }
+
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
 
             myWebView.loadUrl(url);
             return true;
         }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progress.setVisibility(View.GONE);
+        }
     }
 
     private class MyWebChromeClient extends WebChromeClient {
+
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+            progress.setProgress(newProgress);
+
+            if (newProgress<100)
+            {
+                progress.setVisibility(View.VISIBLE);
+
+
+            }
+            if (newProgress == 100)
+            {
+                progress.setVisibility(View.GONE);
+            }
+
+
+        }
 
         //display alert message in Web View
         @Override
@@ -78,7 +146,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public class JavaScriptInterface {
+    private class JavaScriptInterface {
 
         // Instantiate the interface and set the context
         JavaScriptInterface() {
@@ -94,4 +162,17 @@ public class MainActivity extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
+    @Override
+    public void onBackPressed() {
+
+        if (myWebView.canGoBack())
+        {
+            myWebView.goBack();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+
+    }
 }
